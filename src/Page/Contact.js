e55e77194ffaa06b4361eaccfed02dd017d9e68f
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2024-02-23 12:01:11 +0200

    fixed contact page

--- src/Page/Contact.js
+++ src/Page/Contact.js
@@ -5,7 +5,7 @@ import image from "../Images/compp.jpg";
 const Contact = () => {
   const handleEmailClick = () => {
     window.location.href = "mailto:anglebertsh@gmail.com";
-};
+  };
 
   const handlePhoneClick = () => {
     window.location.href = "tel:+250791746914";
@@ -21,10 +21,28 @@ const Contact = () => {
     <div className="container mx-auto p-8">
       <div className="mb-8 h-screen">
         <div className="mt-4 flex flex-col  lg:w-[62%] ml-[12%]  lg:justify-between">
-          <h1 className="text-3xl font-bold mt-[70px]">Contact Me</h1>
+          <h1 className="text-2xl font-bold mt-[70px]">Contact Me</h1>
           <hr className="mt-4 mb-4" />
           <div>
             <img src={image} alt="contact" className="" />
+          </div>
+          <hr className="mt-4 mb-5" />
+          <div>
+            <h2 className="text-2xl font-bold mb-4">Infomation</h2>
+            <hr className="mt-4 mb-4" />
+            <div className="mb-4">
+              <p className="text-1xl font-bold mb-2">
+               Emails:
+              </p>
+              <p className="text-[12px]">anglebertsh@gmail.com</p>
+              <p className="text-[12px]">anglebert.dev@gmail.com</p>
+            </div>
+            <div className="mb-4">
+              <p className="text-1xl font-bold mb-2">
+                Phone:
+              </p>
+              <p className="text-[12px]">+250 791746914</p>
+            </div>
           </div>
           <hr className="mt-4 mb-4" />
           <button
